1e107227ba7a6517911ee75d5a55be42e636961d
Martin Labrecque <mrlabrecque@gmail.com>
2021-08-07 14:50:38 -0400

    trying heroku again

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.mrrlabrecque.foodwithfriends',
   appName: 'food-with-friends',
   webDir: 'www',
   bundledWebRuntime: false
