2c812316c996cf6e95c1b2b5b719bbf1e1ac267f
Martin Labrecque <mrlabrecque@gmail.com>
2021-08-08 10:35:41 -0400

    new capacitor changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,22 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.mrrlabrecque.foodwithfriends',
   appName: 'food-with-friends',
-  webDir: 'www',
-  bundledWebRuntime: false
+  webDir: 'build',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+    },
+    LocalNotifications: {
+      smallIcon: 'ic_stat_icon_config_sample',
+      iconColor: '#CE0B7C',
+    },
+    PushNotifications: {
+      presentationOptions: ['alert', 'sound'],
+    },
+  },
 };
 
 export default config;
